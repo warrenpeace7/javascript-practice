df75ef77fc8a18f59e72e3110b02ae12808ca658
Tyler Thomas <tylerwarrenseven@gmail.com>
2018-03-27 12:11:11 -0600

    Commit a javascript assignment

---
+++ assignment.js
@@ -0,0 +1,79 @@
+Questions
+6 primitive types
+
+Boolean
+Null
+Undefined
+Number
+String
+Symbol (new in ECMAScript 6)
+
+Other language constructs
+Object,array
+
+False when a number zero is considered true 
+True when an object is null, it is considered false
+
+
+let firstName= 'Tyler'
+let lastName= ' Thomas'
+let name= firstName + lastName 
+
+b = 1
+c = 4
+b + c
+=5
+let calculation = b + c
+
+
+let unexpected = firstName + c
+
+1. 
+a.Boolean   Boolean(10 > 9)        // returns true
+b.Null let person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+person = null;        // Now value is null, but type is still an object
+c.Undefined car = undefined;        // Value is undefined, type is undefined
+d.Number var x = 3.14;    // A number with decimals
+e.String let a = 'hello world'
+f.Symbol (new in ECMAScript 6)
+object let person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+array let cars = ["Saab", "Volvo", "BMW"];
+
+
+If you add a number to a work they just combine like this
+b =2
+c = 'hello'
+b+c=
+'2hello'
+
+
+How do you convert a string to a number?
+let num = 15;
+let n = num.toString();
+
+What is the difference between a float and an int?
+Integers and floats are two different kinds of numerical data. 
+An integer (more commonly called an int) is a number without a decimal point. 
+A float is a floating-point number, which means it is a number that has a decimal place. 
+Floats are used when more precision is needed.
+
+Give an example of why you would want to use each one?
+You would want to use a float if you were going for higher accuracy. 
+
+Give an example of adding "Hello" and "World" together to get "Hello World".
+
+let a = 'Hello'
+let b = ' World'
+a + b =
+'Hello World'
+
+
+Provide and example of an array literal?
+let coffees = ['French Roast', 'Colombian', 'Kona'];
+
+Provide an example of an object literal?
+let myObject = {
+    sProp: 'some string value',
+    numProp: 2,
+    bProp: false
+};
